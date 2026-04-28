b5b6b4e27bb4634d851479564496c67d424757ec
Gangadhar <gangadharkuratti790@gmail.com>
2026-04-28 21:42:42 +0530

    initial commit

---
+++ variables_and_datatypes.py
@@ -0,0 +1,27 @@
+# program to understand the concept of variables
+
+Name="Diya"         # here Name is a variable
+Age=20              # here Age is a variable
+Salary=30000.00     # here Salary is a variable
+
+
+# program to understand the concept of Datatypes
+
+Name="Diya"         # string
+Age=20              # integer
+Salary=30000.00     # float
+is_student=True     # boolean
+print(Name)
+print(Age)
+print(Salary)
+print(is_student)
+print(type(Age))    # prints which type of data is stored in variable 'Age'
+
+# program for type conversion
+
+print(Salary)
+print(type(Salary))
+
+Salary=int(Salary)  # before conversion
+print(type(Salary)) # after converting to integer type
+
