e4b01859a03a01426ec70c1e5ef82718e32d6415
Gangadhar <gangadharkuratti790@gmail.com>
2026-05-02 23:19:09 +0530

    added string program

---
+++ lists.py
@@ -0,0 +1,23 @@
+# program to understand the list operation and methods
+
+Numbers=[1,2,3,4,5,6,7,8,9]
+print(Numbers)
+
+Numbers.pop()
+print(Numbers)
+
+Numbers.remove(2)
+print(Numbers)
+
+Numbers.insert(8,10)
+print(Numbers)
+
+Numbers.append(9)
+print(Numbers)
+
+Numbers.reverse()
+print(Numbers)
+
+print(len(Numbers))
+print(Numbers.count(1))
+
